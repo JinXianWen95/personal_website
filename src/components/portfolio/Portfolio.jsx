98cdae69ba1d0026bf4bc9411d1bbcc60687dfd2
jinxw <jinxw@live.it>
2023-10-03 20:35:12 +0200

    version 2

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,47 +1,31 @@
 import React from 'react'
 import './portfolio.css'
 import PORTFOLIO from '../../assets/portfolio.png'
+import websiteIcon from '../../assets/websiteIcon.jpg'
+import remytutor from '../../assets/remytutor.png'
+import tsp from '../../assets/TSPgrasp.png'
 
 
 const data = [
   {
     id: 1,
-    image: PORTFOLIO,
-    title: 'Title1',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/Alien_pixels'
+    image: websiteIcon,
+    title: 'Personal Website',
+    github: 'https://github.com/JinXianWen95/personal_website'
   },
 
   {
     id: 2,
-    image: PORTFOLIO,
-    title: 'Title2',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/Alien_pixels'
+    image: remytutor,
+    title: 'Teaching Platform RemyTutor',
+    github: 'https://github.com/JinXianWen95/RemyTutor'
   },
   
   {
     id: 3,
-    image: PORTFOLIO,
-    title: 'Title3',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/Alien_pixels'
-  },
-
-  {
-    id: 4,
-    image: PORTFOLIO,
-    title: 'Title4',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/Alien_pixels'
-  },
-
-  {
-    id: 5,
-    image: PORTFOLIO,
-    title: 'Title5',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/Alien_pixels'
+    image: tsp,
+    title: 'TSP Cplex Solver',
+    github: 'https://github.com/JinXianWen95/TSP_CPLEX_SOLVER'
   },
 ]
 
@@ -54,7 +38,7 @@ export const Portfolio = () => {
       <div className='container portfolio__container'>
         
         {
-          data.map(({id,image, title, github, demo}) => {
+          data.map(({id,image, title, github}) => {
             return (
               <article key={id} className='portfolio__item'>
                 <div className='portfolio__item-image'>
@@ -63,7 +47,6 @@ export const Portfolio = () => {
               <h3>{title}</h3>
                 <div className='portfolio__item-cta'>
                   <a href={github} className='btn' target='_blank'>Github</a>
-                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
                 </div>
               </article>
             )
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -16,38 +16,35 @@ export const Services = () => {
 
           <ul className='service__list'>
             <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+              <p>
+                I was the Software developer and mainly responsible of collecting CodeArts pipeline statistics.
+                The CodeArts Pipeline is essentially a visual automated task scheduling platform, 
+                which needs to be used in conjunction with the automated tasks of compilation and build, code check, cloud testing,
+                deployment and other services in the software development cycle.<br/>
+                My main contribution was to collect the execution statistics of pipeline in real-time
+                and provide them to front-end team, therefore the users know quickly the state of their software.
+                <div>
+                  <p>SpringBoot</p>
+                  <p>RabbitMQ</p>
+                  <p>Redis</p>
+                  <p>Mybatis</p>
+                  <p>PostgreSQL</p>
+                  <p>MySQL</p>
+                </div>
+              </p>
             </li>
           </ul>
         </article>
 
         <article className='service'>
           <div className='service__head'>
-            <h3>Banco BPM</h3>
+            <h3>Reply SRL</h3>
           </div>
 
           <ul className='service__list'>
             <li>
               <p>
-                <h4>The BBPM bank is the third largest retail and corporate banking conglomerate in Italy.</h4>
-                Working for Reply SRL I have been responsible for design and implementation of offloading pipeline made of change data capture tool from mainframe, event streaming platform and relational
+                I have been responsible for design and implementation of offloading pipeline made of change data capture tool from mainframe, event streaming platform and relational
                 database for a main Italian banking group to address PSD2 regulation and open banking challenge. <br/>
                 As result the data on Oracle Database is much compact and faster to retrieve compared to DB2 Database. It has been observed an improvement of 30% in select query speed.
                 Furthermore it provides additional information such as the history of account balance.
@@ -70,29 +67,18 @@ export const Services = () => {
 
         <article className='service'>
           <div className='service__head'>
-            <h3>Content Creation</h3>
+            <h3>University of Padua</h3>
           </div>
 
           <ul className='service__list'>
             <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+              <p>
+              During my university journey I have been focusing on Big Data technology and algorithm efficiency. Furthermore I have been part of different projects
+              and have collaborated with a lot brilliant mates.<br/>
+              I have implemented the Traveler Salesman Problem Solver using IBM ILOG CPLEX Optimization Studio at the Operational Research course.<br/>
+              Enjoyed the team work with the realization of Teaching Platform Web Application. <br/>
+              Concluded the Master with a thesis analyzing and improving k-Center Clustering with MapReduce Algorithm.
+              </p>
             </li>
           </ul>
         </article>
